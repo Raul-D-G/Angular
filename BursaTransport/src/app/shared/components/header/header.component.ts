ef07b588086d57ee022baf8b1d7e2d6e203c33ab
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2021-02-03 20:53:54 +0200

    verificare timp token

--- BursaTransport/src/app/shared/components/header/header.component.ts
+++ BursaTransport/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { ProgressBarService } from './../../services/progress-bar.service';
 import { Component, OnInit } from '@angular/core';
 import { NgProgress } from '@ngx-progressbar/core';
+import { AlertService } from 'ngx-alerts';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +11,16 @@ import { NgProgress } from '@ngx-progressbar/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private progress: NgProgress, public progressBar: ProgressBarService) { }
+  constructor(private progress: NgProgress, public progressBar: ProgressBarService, public authService: AuthService,
+    private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.progressBar.progressRef = this.progress.ref('progressBar');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.alertService.success("Deconectare!");
   }
 
 }
--- BursaTransport/src/app/shared/services/auth.service.ts
+++ BursaTransport/src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   authUrl = "http://localhost:3000/api/users";
+  helper = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
@@ -23,9 +25,17 @@ export class AuthService {
     )
   }
 
-
   register(model: any) {
     return this.http.post(this.authUrl, model);
   }
+
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.helper.isTokenExpired(token);
+  }
+
+
+
 
 }
