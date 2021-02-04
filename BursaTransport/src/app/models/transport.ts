a72fe02cafe0e23f28960334673b5a60f932a02b
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2021-02-04 22:04:14 +0200

    add transport service, transport class

--- BursaTransport/src/app/models/transport.ts
+++ BursaTransport/src/app/models/transport.ts
@@ -1,2 +1,19 @@
 export class Transport {
+    id: number;
+    taraIncarcare: string;
+    orasIncarcare: string;
+    taraDescarcare: string;
+    orasDescarcare: string;
+    pret: number;
+    km: number;
+
+    constructor(id: number, taraIncarcare: string, orasIncarcare: string, taraDescarcare: string, orasDescarcare: string, pret: number, km: number) {
+        this.id = id;
+        this.taraIncarcare = taraIncarcare;
+        this.orasIncarcare = orasIncarcare;
+        this.taraDescarcare = taraDescarcare;
+        this.orasDescarcare = orasDescarcare;
+        this.pret = pret;
+        this.km = km;
+    }
 }
