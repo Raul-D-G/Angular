b1f868d6fed97fb8d4dadf9ad02733b990ecb4e6
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2021-05-28 16:08:21 +0300

    organizareauth guard

--- BursaTransport/src/app/guards/auth-guard.guard.ts
+++ BursaTransport/src/app/guards/auth-guard.guard.ts
@@ -1,13 +1,6 @@
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-  Router,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
