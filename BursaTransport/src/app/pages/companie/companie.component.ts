e17fd65376787570b6fc7269ebe298a66bfb51e8
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2021-06-24 20:44:48 +0300

    pagina companie 83%

--- BursaTransport/src/app/pages/companie/companie.component.ts
+++ BursaTransport/src/app/pages/companie/companie.component.ts
@@ -8,6 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanieComponent implements OnInit {
   constructor(public authService: AuthService) {}
-
+  collapsed = true;
   ngOnInit(): void {}
 }
